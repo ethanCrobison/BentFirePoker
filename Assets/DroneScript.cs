e0347ad255b577970d93fae2ea9b09564bf2b9cc
Ethan <ethanCrobison@gmail.com>
2016-02-01 02:41:26 -0600

    initial commit

---
+++ Assets/DroneScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneScript : MonoBehaviour {
+	public float speed = 2.0F;
+	public float thresh = 5.0F;
+	public GameObject thePlayer;
+
+	void Update () {
+		if (Vector3.Distance (thePlayer.transform.position, this.transform.position) > thresh) {
+			Debug.Log ("Away");
+		}
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float horizontalSpeed = 2.0F;
+	public float verticalSpeed = 2.0F;
+
+	void Update () {
+		float h = horizontalSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+		float v = verticalSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+		transform.Translate (h, v, 0);
+		//Debug.Log (transform.position);
+	}
+}
