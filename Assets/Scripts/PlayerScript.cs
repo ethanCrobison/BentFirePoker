7401b75a7bc5b4a5555ccf92b9415a3d1a279a95
Ethan <ethanCrobison@gmail.com>
2016-02-03 18:05:09 -0600

    added rudimentary abilities

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,14 +9,36 @@ public class PlayerScript : MonoBehaviour {
 	public event Action SpawnMinion = delegate {};
 
 	void Update () {
-		float h = horizontalSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-		float v = verticalSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-		transform.Translate (h, v, 0);
-
 		if (Input.GetButtonDown ("Spawn")) {
 			SpawnMinion.Invoke ();
 		}
+		if (Input.GetButtonDown ("Ability1")) {
+			ability1 ();
+		} else if (Input.GetButtonDown ("Ability2")) {
+			ability2 ();
+		} else if (Input.GetButtonDown ("Ability3")) {
+		 	ability3 ();
+		}
 	}
 
+	void FixedUpdate() {
+		float h = horizontalSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+		float v = verticalSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+		transform.Translate (h, v, 0);
+	}
+
+	void ability1 () {
+		changeColor (Color.cyan);
+	}
+	void ability2 () {
+		changeColor (Color.green);
+	}
+	void ability3 () {
+		changeColor (Color.magenta);
+	}
+
+	void changeColor (Color color) {
+		this.GetComponent<SpriteRenderer> ().color = color;
+	}
 
 }
