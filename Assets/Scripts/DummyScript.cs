b0a812f054dffa526bc47315b43bef61fdaab926
Ethan <ethanCrobison@gmail.com>
2016-02-10 16:59:46 -0600

    rearranged

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class DummyScript : MonoBehaviour {
 
-	public int hp;
+	private int hp;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake() {
+		this.hp = 100;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (hp <= 0) {
 			Destroy (gameObject);
--- Assets/Scripts/MinionManager.cs
+++ Assets/Scripts/MinionManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class MinionManager : MonoBehaviour {
 
 	public GameObject MinionPrefab;
 	public GameObject WardPrefab;
+
+	public event Action TargetSet;
 
 	private float minX = -4;
 	private float maxX =  4;
@@ -12,24 +14,29 @@ public class MinionManager : MonoBehaviour {
 	private float maxY =  4;
 
 	void Start () {
-		GameObject player = GameObject.FindGameObjectWithTag ("Player");
-		PlayerScript ps = player.GetComponent<PlayerScript> ();
+		PlayerScript ps = this.GetComponent<PlayerScript>();
 		ps.SpawnMinion += SpawnNewMinion;
 		ps.SpawnWard += SpawnNewWard;
 	}
 
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("hi");
+		}
+	} 
+
 	private void SpawnNewMinion() {
-		
 		GameObject minion = GameObject.Instantiate (MinionPrefab);
-		var x = this.transform.position.x + Random.Range (minX, maxX);
-		var y = this.transform.position.y + Random.Range (minY, maxY);
-		minion.transform.position = new Vector2 (x, y);
+		minion.transform.position = NewSpawnPoint ();
 	}
 
 	private void SpawnNewWard () {
 		GameObject minion = GameObject.Instantiate (WardPrefab);
-		var x = this.transform.position.x + Random.Range (minX, maxX);
-		var y = this.transform.position.y + Random.Range (minY, maxY);
-		minion.transform.position = new Vector2 (x, y);
+		minion.transform.position = NewSpawnPoint ();
 	}
+	private Vector2 NewSpawnPoint () {
+		var x = this.transform.position.x + UnityEngine.Random.Range (minX, maxX);
+		var y = this.transform.position.y + UnityEngine.Random.Range (minY, maxY);
+		return new Vector2 (x, y);
+	}		
 }
