25f26a3a07fbf94663dcb9658c0385a0bce0b163
Ethan <ethanCrobison@gmail.com>
2016-02-24 17:52:01 -0600

    changed wall sprite and background

---
+++ Assets/Scripts/EventsScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class EventsScript : MonoBehaviour {
+
+	public event Action EventNewMinion = delegate {};
+	public event Action EventTreasureCollected = delegate {};
+
+	public void NewMinion() {
+		// TODO any validation?
+		this.EventNewMinion.Invoke ();
+	}
+
+	public void TreasureCollected () {
+		this.EventTreasureCollected.Invoke ();
+	}
+
+}
---
+++ Assets/Scripts/UI/MinionUIScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MinionUIScript : MonoBehaviour {
+
+	private int _OrdinaryMinionCount = 0;
+
+	void Start () {
+		var eventBus = GameObject.Find ("EventBus").GetComponent<EventsScript>();
+		eventBus.EventNewMinion += OnMinionSpawn;
+		UpdateMinionCount ();
+	}
+
+	private void OnMinionSpawn() {
+		_OrdinaryMinionCount += 1;
+		UpdateMinionCount ();
+	}
+
+	private void UpdateMinionCount () {
+		GameObject minionTextObject = this.gameObject;
+		Text textComponent = minionTextObject.GetComponent<Text> ();
+		textComponent.text = string.Format ("Minions: {0}", _OrdinaryMinionCount);
+	}
+
+}
