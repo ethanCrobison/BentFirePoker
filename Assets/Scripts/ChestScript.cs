0cfaae3de8ba7672645b36a52a8a55f63350d3fd
itsmichaelwang <michaelwang2016@u.northwestern.edu>
2016-02-28 18:28:08 -0600

    Add interactive chests

---
+++ Assets/Scripts/ChestScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestScript : MonoBehaviour {
+
+	public Sprite openedChest;
+
+	private enum State
+	{
+		CLOSED,
+		OPENED
+	};
+	private State STATE;
+
+	// Use this for initialization
+	void Start () {
+		STATE = State.CLOSED;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D collider) {
+		if (STATE == State.OPENED) {
+			return;
+		}
+
+		if (collider.gameObject.tag == "Player") {
+			STATE = State.OPENED;
+			this.GetComponent<SpriteRenderer> ().sprite = openedChest;
+			this.GetComponent<AudioSource> ().Play ();
+		}
+	}
+}
