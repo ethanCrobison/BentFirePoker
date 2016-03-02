f690e9e4e0ae14ef63bc264dc4042c9f1544b1be
Ethan <ethanCrobison@gmail.com>
2016-03-02 16:53:00 -0600

    added treasure manager files lol

---
+++ Assets/Scripts/TreasureManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureManager : MonoBehaviour {
+
+	private int TreasureCount = 11;
+	private EventsScript EventBus;
+
+	// Use this for initialization
+	void Start () {
+		EventBus = GameObject.Find ("EventBus").GetComponent<EventsScript> ();
+		EventBus.EventTreasureCollected += IncTreasure;
+		EventBus.EventNewMinion += DecTreasure;
+	}
+	
+	private void IncTreasure() {
+		TreasureCount++;
+	}
+
+	private void DecTreasure() {
+		TreasureCount--;
+	}
+
+	public bool Capacity() {
+		return this.TreasureCount > 0;
+	}
+}
