42d89b1ab0bfcd9aff1297bb91690fe7c53f499d
Ethan <ethanCrobison@gmail.com>
2016-03-09 16:59:44 -0600

    coin disappears faster

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -5,7 +5,7 @@ public class CoinScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Destroy (this.gameObject, 10.0F);
+		Destroy (this.gameObject, 0.3F);
 	}
 	
 	// Update is called once per frame
